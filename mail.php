72610e598c1ae945ed377843fde9e65b253b89aa
An, Albert <alban@visa.com>
2017-04-21 20:10:44 -0700

    main added

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ /*$sth = $this->db->prepare('SELECT * FROM `members` WHERE `index` = :id');
+                $sth->execute(array(':id'=> $member_id));
+                $member_info= $sth->fetch();
+                       */
+        $to      = "contendera@gmail.com";
+        $subject = 'confirmation email from meetingmeeting.com';
+        $message = 'Welcome';
+        $headers = 'From: services@meetingmeeting.com' . "\r\n" .
+        'Reply-To: services@meetingmeeting.com' . "\r\n" .
+         'X-Mailer: PHP/' . phpversion();
+          mail($to, $subject, $message, $headers);
