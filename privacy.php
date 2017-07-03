2f2d6f50e922aeef6c3662ba7641c8154951e47a
An, Albert <alban@visa.com>
2017-07-03 13:23:42 -0700

    added privacy policy

---
+++ privacy.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--
+Hi this is Albert's resume website!  I'm so exited all the time!!!!!!!! YYYAAYAYAYAYAYAYA!!!!!
+I want you to FFFFEEEEEEL my ENERGY! awWWWW Yeah!
+-->
+<html>
+    <head>
+        <title>Albert An (Joong Hyuk An)</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        
+        <link rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap.css" >
+        
+         <script  src="libs/jquery-1.11.3.min.js"></script>
+        <script  src="libs/bootstrap/js/bootstrap.min.js"></script>
+        
+           <script  src="js/main.js"></script>
+        
+        
+        
+    </head>
+    <body>
+        
+        <h1>Privacy Policy</h1>
+        <div class="fullscreen-bg">
+            
+            <p>
+
+
+
+
+             </p>
+          
+        </div>
+        
+        
+   
+        
+    </body>
+</html>
