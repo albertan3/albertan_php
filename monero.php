cb87bb083682e3abf24e6e5318ef3da8de56cc11
Albert <contendera@gmail.com>
2017-11-21 12:05:27 -0800

    monero

---
+++ monero.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+Hi this is Albert's resume website!  I'm so exited all the time!!!!!!!! YYYAAYAYAYAYAYAYA!!!!!
+I want you to FFFFEEEEEEL my ENERGY! awWWWW Yeah!
+-->
+<html>
+    <head>
+        <title>Albert An (Joong Hyuk An)</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        
+        <link rel="stylesheet" type="text/css" href="libs/bootstrap/css/bootstrap.css" >
+        
+         <script  src="libs/jquery-1.11.3.min.js"></script>
+        <script  src="libs/bootstrap/js/bootstrap.min.js"></script>
+        
+           <script  src="js/main.js"></script>
+        
+        
+        
+    </head>
+    <body>
+    <!--10wC8GMgzJLQRtWR6H1DvObK8SgJAczr-->
+
+    <script src="https://authedmine.com/lib/simple-ui.min.js" async></script>
+                <div class="coinhive-miner" 
+                    style="width: 256px; height: 310px"
+                    data-key="10wC8GMgzJLQRtWR6H1DvObK8SgJAczr">
+                    <em>Loading...</em>
+                </div>
+    </body>
+</html>
